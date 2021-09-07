0f78f0ae5956e7d6c2fe8e2f6d290f59962c76bd
unknown <kimhan1113@naver.com>
2021-09-08 00:30:54 +0900

    django course 31 commit

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('profileapp:hello_world')
+    template_name = 'profileapp/create.html'
+
+    # model 에서는 user가 있는데 form에서는 user는 안쓰니깐 customizing 이 필요해서 넣은 함수
+    def form_valid(self, form):
+        temp_profile = form.save(commit=False)
+        temp_profile.user = self.request.user
+        temp_profile.save()
+        return super().form_valid(form)
