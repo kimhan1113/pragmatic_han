b7b10d421c3b1a341a669ea5b73548b9ab1b587d
unknown <kimhan1113@naver.com>
2021-09-08 14:41:48 +0900

    django course 33 commit

--- profileapp/views.py
+++ profileapp/views.py
@@ -14,8 +14,11 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
+
+    def get_success_url(self):
+        return reverse_lazy('accountapp:detail', kwargs={'pk': self.object.user.pk})
 
     # model 에서는 user가 있는데 form에서는 user는 안쓰니깐 customizing 이 필요해서 넣은 함수
     def form_valid(self, form):
@@ -30,5 +33,9 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     form_class = ProfileCreationForm
     context_object_name = 'target_profile'
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse_lazy('accountapp:detail', kwargs={'pk': self.object.user.pk})
+
